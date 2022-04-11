e0a849ac151d5492bcc4871f708936ba9b960d5c
DIANACH <dianepatrich@gmail.com>
2022-04-10 20:24:11 -0500

    npm run build

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className='footerContainer'>
-        <p className="footerParagraph">
-        © Copyright 2022. All rights reserved. Developed by Diana Patricia Chavez Realpe
-        </p>
-      
+    <div className="footerContainer">
+      <p className="footerParagraph">
+        © Copyright 2022. All rights reserved. Developed by Diana Patricia
+        Chavez Realpe
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
