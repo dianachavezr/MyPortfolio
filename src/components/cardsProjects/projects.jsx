da4d8b3a2be474c40f8e5bbe25e7e948a8a11b1e
DIANACH <dianepatrich@gmail.com>
2022-06-05 14:10:10 -0500

    details updated

--- src/components/cardsProjects/projects.jsx
+++ src/components/cardsProjects/projects.jsx
@@ -1,4 +1,3 @@
-
 import Project1 from "../../assets/images/comoPerro5.1.jpg";
 import project1 from "../../assets/images/dogAngel.png";
 import Project2 from "../../assets/images/covidApi.jpg";
@@ -9,14 +8,27 @@ import Project4 from "../../assets/images/cena4.jpg";
 import project4 from "../../assets/images/cena.png";
 import Project5 from "../../assets/images/todolist.png";
 import project5 from "../../assets/images/todolist.png";
-
+import Project6 from "../../assets/images/programate.png";
+import project6 from "../../assets/images/programate1.png";
 
 const projects = {
-  
   projecstPortfolio: [
-
     {
       id: 1,
+      img: Project6,
+      img3: project6,
+      title: "Proyecto Legacy Selección",
+      description: "Final project",
+      description2:
+        "This was the final Bootcamp project were we were given an ongoing project and we took part of the 2nd fase, my given position was Front-End Lider",
+      description3: "MERN Stack",
+
+      linkGitHub:
+        "https://github.com/ProgramateAcademyE/Legacy-Proceso-de-Seleccion-front/tree/DEV2",
+      link: "https://legacy-proceso-seleccion.netlify.app/",
+    },
+    {
+      id: 2,
       img: Project5,
       img3: project5,
       title: "TodoList",
@@ -25,11 +37,12 @@ const projects = {
         "This project was made as part of our MongoDB training and to understand the conexion process between Front-End and Back-End",
       description3: "ReactJs, MongoDB.",
 
-      linkGitHub: "https://github.com/dianachavezr/-todolistprojectcrud-front-end",
+      linkGitHub:
+        "https://github.com/dianachavezr/-todolistprojectcrud-front-end",
       link: "https://todolistcrudmongodb.netlify.app/",
     },
     {
-      id: 2,
+      id: 3,
       img: Project1,
       img3: project1,
       title: "Andika 3D",
@@ -42,7 +55,7 @@ const projects = {
       link: "https://e-commerceandika3d.netlify.app/",
     },
     {
-      id: 3,
+      id: 4,
       img: Project4,
       img3: project4,
       title: "Rick & Morty API",
@@ -54,9 +67,9 @@ const projects = {
       linkGitHub: "https://github.com/dianachavezr/ApiRickAndMorty",
       link: "https://apirickandmortyproject.netlify.app/",
     },
-  
+
     {
-      id: 4,
+      id: 5,
       img: Project2,
       img3: project2,
       title: "Covid APIRest",
@@ -69,7 +82,7 @@ const projects = {
       link: "https://covidapirestproject.netlify.app/",
     },
     {
-      id: 5,
+      id: 6,
       img: Project3,
       img3: project3,
       title: "Linea1 Vacunación",
@@ -81,60 +94,6 @@ const projects = {
       linkGitHub: "https://github.com/dianachavezr/Linea1Vacunacion",
       link: "https://dianachavezr.github.io/Linea1Vacunacion ",
     },
-    {
-      id: 1,
-      img: Project5,
-      img3: project5,
-      title: "TodoList",
-      description: "ReactJs & MongoDB Project",
-      description2:
-        "This project was made as part of our MongoDB training and to understand the conexion process between Front-End and Back-End",
-      description3: "ReactJs, MongoDB.",
-
-      linkGitHub: "https://github.com/dianachavezr/-todolistprojectcrud-front-end",
-      link: "https://todolistcrudmongodb.netlify.app/",
-    },
-    {
-      id: 2,
-      img: Project1,
-      img3: project1,
-      title: "Andika 3D",
-      description: "E-Commerce ReactJs Project",
-      description2:
-        "This project was carried out as part of our education process where we would donate a project to a new and growing company and thus contribute our grain of sand by collaborating on new projects.",
-      description3: "ReactJs, CSS",
-
-      linkGitHub: "https://github.com/dianachavezr/E-commerceAndika3D",
-      link: "https://dianachavezr.github.io/E-commerceAndika3D/",
-    },
-    {
-      id: 3,
-      img: Project4,
-      img3: project4,
-      title: "Rick & Morty API",
-      description: "ReactJs Project",
-      description2:
-        "This project was made as first ReactJs project in as well as looking to show how to fetch an API Rest",
-      description3: "ReactJs, Axios, CSS Bootstrap.",
-
-      linkGitHub: "https://github.com/dianachavezr/ApiRickAndMorty",
-      link: "https://dianachavezr.github.io/ApiRickAndMorty/",
-    },
-  
-    {
-      id: 4,
-      img: Project2,
-      img3: project2,
-      title: "Covid APIRest",
-      description: "Covid APIRest JavaScript Project",
-      description2:
-        "This project was made in order to demonstrate the knowledge of API fetch with JavaScript.",
-      description3: "HTML,CSS,JavaScript.",
-
-      linkGitHub: "https://github.com/dianachavezr/E-commerceAndika3D",
-      link: "https://covidapirestproject.netlify.app/",
-    },
-
   ],
 };
 
